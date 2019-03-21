c7412b3719221323c00fc7d2d925fabee9dbd40d
krystianbialy <krystian.bialy@onet.pl>
2019-03-21 20:03:17 +0100

    amendments

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 //===== VARIABLES =====
 const gulp = require('gulp');
+const browserSync = require("browser-sync").create();
 const sass = require('gulp-sass');
 
 //===== TASKS =====
-gulp.task('sass', () => {
-  return gulp.src('./sass/main.scss')
+gulp.task('serve', function() {
+  browserSync.init({
+    server: "./"
+  });
+  gulp.watch('sass/*.scss', gulp.series('sass'));
+  gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+  return gulp.src('sass/*.scss')
     .pipe(sass({
       outputStyle: 'compressed'
     }))
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', () => {
-  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
-});
+gulp.task('default', gulp.parallel('serve'));
